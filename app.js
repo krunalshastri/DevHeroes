778b0f0e9164ed800674d086a4d1b0f8daf76952
krunalshastri00 <krunalshastri00@gmail.com>
2021-12-15 10:20:25 +0530

    Prepared for deployment

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const dotEnv = require('dotenv').config();
 const User = require('./Models/userModel');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 app.use(express.json());
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send('API running!!');
-});
 
 app.use('/dev', require('./Routes/users'));
 app.use('/dev', require('./Routes/profile'));
 app.use('/dev', require('./Routes/auth'));
 app.use('/dev/posts', require('./Routes/posts'));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
-
-// 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
