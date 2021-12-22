af563c853daf3b92671314f030c0147d0ad3d397
krunalshastri00 <krunalshastri00@gmail.com>
2021-12-22 23:31:50 +0530

    Added changes to navbar layout

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
@@ -36,11 +35,11 @@ function App() {
       <Router>
         <Fragment>
           <Navbar />
-          <div className='wrapper'>
-            <Route exact path='/' component={Landing} />
-            <Route exact path='/login' component={Login} />
-            <Route exact path='/register' component={Register} />
-          </div>
+          {/* <div className='wrapper'> */}
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+          {/* </div> */}
           <section className='container'>
             <Switch>
               <Route exact path='/profiles' component={Profiles} />
